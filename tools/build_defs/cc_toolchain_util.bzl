bazel-contrib/rules_foreign_cc

commit 604f1e1c9f63d122365041e3a8e0afba41dcb55b

Guard cc_common.configure_features with a version check (#270)
This PR adds @bazel_version repository which contains single def.bzl
file which contains a BAZEL_VERSION constant that can be used to perform
conditional logic bazed on the Bazel version.

As a result, rules_foreing_cc are now compatible with Bazel 0.24 - 0.27
(at least regarding configure_features :)

tools/build_defs/cc_toolchain_util.bzl
@@ -12,6 +12,8 @@ load(
     "C_COMPILE_ACTION_NAME",
 )
 load("@bazel_skylib//lib:collections.bzl", "collections")
+load("@bazel_skylib//lib:versions.bzl", "versions")
+load("@bazel_version//:def.bzl", "BAZEL_VERSION")
 
 LibrariesToLinkInfo = provider(
     doc = "Libraries to be wrapped into CcLinkingInfo",
@@ -55,6 +57,22 @@ def _to_depset(element):
         return depset()
     return depset(element)
 
+def _configure_features(ctx, cc_toolchain):
+    if (len(BAZEL_VERSION) == 0 or
+        versions.is_at_least("0.25.2", BAZEL_VERSION)):
+        return cc_common.configure_features(
+            ctx = ctx,
+            cc_toolchain = cc_toolchain,
+            requested_features = ctx.features,
+            unsupported_features = ctx.disabled_features,
+        )
+    else:
+        return cc_common.configure_features(
+            cc_toolchain = cc_toolchain,
+            requested_features = ctx.features,
+            unsupported_features = ctx.disabled_features,
+        )
+
 def _create_libraries_to_link(ctx, files):
     libs = []
 
@@ -66,11 +84,10 @@ def _create_libraries_to_link(ctx, files):
     names = collections.uniq(static_map.keys() + pic_static_map.keys() + shared_map.keys() + interface_map.keys())
 
     cc_toolchain = find_cpp_toolchain(ctx)
-    feature_configuration = cc_common.configure_features(
+
+    feature_configuration = _configure_features(
         ctx = ctx,
         cc_toolchain = cc_toolchain,
-        requested_features = ctx.features,
-        unsupported_features = ctx.disabled_features,
     )
 
     for name_ in names:
@@ -178,11 +195,9 @@ def targets_windows(ctx, cc_toolchain):
         cc_toolchain - optional - Cc toolchain
     """
     toolchain = cc_toolchain if cc_toolchain else find_cpp_toolchain(ctx)
-    feature_configuration = cc_common.configure_features(
+    feature_configuration = _configure_features(
         ctx = ctx,
         cc_toolchain = toolchain,
-        requested_features = ctx.features,
-        unsupported_features = ctx.disabled_features,
     )
 
     return cc_common.is_enabled(
@@ -208,8 +223,6 @@ def get_env_vars(ctx):
     feature_configuration = cc_common.configure_features(
         ctx = ctx,
         cc_toolchain = cc_toolchain,
-        requested_features = ctx.features,
-        unsupported_features = ctx.disabled_features,
     )
     copts = ctx.attr.copts if hasattr(ctx.attr, "copts") else []
 
@@ -240,8 +253,6 @@ def get_tools_info(ctx):
     feature_configuration = cc_common.configure_features(
         ctx = ctx,
         cc_toolchain = cc_toolchain,
-        requested_features = ctx.features,
-        unsupported_features = ctx.disabled_features,
     )
 
     return CxxToolsInfo(
@@ -272,8 +283,6 @@ def get_flags_info(ctx):
     feature_configuration = cc_common.configure_features(
         ctx = ctx,
         cc_toolchain = cc_toolchain_,
-        requested_features = ctx.features,
-        unsupported_features = ctx.disabled_features,
     )
 
     copts = (ctx.fragments.cpp.copts + ctx.fragments.cpp.conlyopts) or []

for_workspace/bazel_version.bzl
@@ -1,0 +1,10 @@
+def _store_bazel_version(repository_ctx):
+    bazel_version = native.bazel_version
+    if len(bazel_version) == 0:
+        print("You're using development build of Bazel, make sure it's recent - version check is disabled.")
+    repository_ctx.file("BUILD", "exports_files(['def.bzl'])")
+    repository_ctx.file("def.bzl", "BAZEL_VERSION='" + bazel_version + "'")
+
+bazel_version = repository_rule(
+    implementation = _store_bazel_version,
+)

workspace_definitions.bzl
@@ -4,6 +4,7 @@ load(
     "//tools/build_defs/shell_toolchain/toolchains:ws_defs.bzl",
     shell_toolchain_workspace_initalization = "workspace_part",
 )
+load("//for_workspace:bazel_version.bzl", "bazel_version")
 
 def _read_build_options_impl(rctx):
     rctx.file("BUILD.bazel", "\n".join(
@@ -91,6 +92,7 @@ def rules_foreign_cc_dependencies(
     """
     repositories()
     _read_build_options(name = "foreign_cc_platform_utils")
+    bazel_version(name="bazel_version")
 
     shell_toolchain_workspace_initalization(
         additonal_shell_toolchain_mappings,

commit 46f16cb4bb5bc13ecdfe389103ce3426215f8b9a

Use forward compatible version of _configure_features
This PR fixes the bug introduced in https://github.com/bazelbuild/rules_foreign_cc/pull/270.

hlopko@ should not be allowed to code undercaffeinated.

tools/build_defs/cc_toolchain_util.bzl
@@ -220,7 +220,7 @@ def create_linking_info(ctx, user_link_flags, files):
 
 def get_env_vars(ctx):
     cc_toolchain = find_cpp_toolchain(ctx)
-    feature_configuration = cc_common.configure_features(
+    feature_configuration = _configure_features(
         ctx = ctx,
         cc_toolchain = cc_toolchain,
     )
@@ -250,7 +250,7 @@ def get_tools_info(ctx):
         ctx - rule context
     """
     cc_toolchain = find_cpp_toolchain(ctx)
-    feature_configuration = cc_common.configure_features(
+    feature_configuration = _configure_features(
         ctx = ctx,
         cc_toolchain = cc_toolchain,
     )
@@ -280,7 +280,7 @@ def get_flags_info(ctx):
         ctx - rule context
     """
     cc_toolchain_ = find_cpp_toolchain(ctx)
-    feature_configuration = cc_common.configure_features(
+    feature_configuration = _configure_features(
         ctx = ctx,
         cc_toolchain = cc_toolchain_,
     )