9c916f0856c67dd2f1f0aaffca8339ba3411d4c8
Danny Wolf <wolfd@users.noreply.github.com>
2021-03-15 21:03:16 -0700

    Example usage of __TIME__

--- examples/cmake_defines/lib_a/lib_a.cpp
+++ examples/cmake_defines/lib_a/lib_a.cpp
@@ -1,3 +1,12 @@
 #ifndef FOO
 #error FOO is not defined
 #endif
+
+#define XSTR(x) STR(x)
+#define STR(x) #x
+#pragma message "The value of __TIME__: " XSTR(__TIME__)
+
+// Should return "redacted"
+const char *getBuildTime(void) {
+    return __TIME__;
+}
