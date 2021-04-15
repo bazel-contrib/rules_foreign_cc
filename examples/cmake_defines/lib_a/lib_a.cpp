d263a7803fcb100184d8075303d21f34daf4fec7
James Sharpe <james.sharpe@zenotech.com>
2021-04-16 00:21:09 +0100

    Add compile check that __TIME__ is correctly redacted

--- examples/cmake_defines/lib_a/lib_a.cpp
+++ examples/cmake_defines/lib_a/lib_a.cpp
@@ -6,6 +6,13 @@
 #define STR(x) #x
 #pragma message "The value of __TIME__: " XSTR(__TIME__)
 
+#define STATIC_ASSERT( condition, name )\
+    typedef char assert_failed_ ## name [ (condition) ? 1 : -1 ];
+
+void foo() {
+STATIC_ASSERT(__TIME__ == "redacted", time_must_be_redacted);
+}
+
 // Should return "redacted"
 const char *getBuildTime(void) {
     return __TIME__;
