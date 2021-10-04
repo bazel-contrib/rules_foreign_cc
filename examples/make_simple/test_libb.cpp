34083ab70a0b4b9e1b230682baf3283d9d9bb4a2
Fabian Meumertzheim <fabian@meumertzhe.im>
2021-10-04 23:10:03 +0200

    Add integration test coverage for make flag passing
    This requires making the make_simple Makefile more realistic by
    
    * using CXX and forwarding it to the wrapper;
    * using CXXFLAGS instead of CXX_FLAGS and not overwriting its contents.

--- examples/make_simple/test_libb.cpp
+++ examples/make_simple/test_libb.cpp
@@ -10,5 +10,9 @@ int main(int argc, char* argv[])
   if (result != "Hello from LIBA!") {
     throw std::runtime_error("Wrong result: " + result);
   }
+  double math_result = hello_math(0.5);
+  if (math_result < 1.047 || math_result > 1.048) {
+    throw std::runtime_error("Wrong math_result: " + std::to_string(math_result));
+  }
   std::cout << "Everything's fine!";
 }
