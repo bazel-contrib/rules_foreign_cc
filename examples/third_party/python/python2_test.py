f2a23fbf29e93e794ecfa3609b73880d64656e58
UebelAndre <github@uebelandre.com>
2021-12-07 11:14:38 -0800

    Remove `BUILD` files from source globs in examples

--- examples/third_party/python/python2_test.py
+++ examples/third_party/python/python2_test.py
@@ -5,4 +5,4 @@ import platform
 if __name__ == "__main__":
 
     assert('python2_test.runfiles/python2/python2/bin/' in sys.executable)
-    assert(platform.python_version() == "2.7.9")
+    assert(platform.python_version() == "2.7.18")
