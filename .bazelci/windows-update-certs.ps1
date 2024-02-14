075c4be5600b11f3d085f735b9fab5696edc860c
Misha Zharov <mishazharov1@gmail.com>
2024-02-14 00:37:39 -0800

    Don't specify openssl path

--- .bazelci/windows-update-certs.ps1
+++ .bazelci/windows-update-certs.ps1
@@ -2,5 +2,5 @@ cd $env:USERPROFILE;
 Invoke-WebRequest https://curl.haxx.se/ca/cacert.pem -OutFile $env:USERPROFILE\cacert.pem;
 $plaintext_pw = 'PASSWORD';
 $secure_pw = ConvertTo-SecureString $plaintext_pw -AsPlainText -Force;
-& 'C:\Program Files\OpenSSL-Win64\bin\openssl.exe' pkcs12 -export -nokeys -out certs.pfx -in cacert.pem -passout pass:$plaintext_pw;
+& openssl.exe pkcs12 -export -nokeys -out certs.pfx -in cacert.pem -passout pass:$plaintext_pw;
 Import-PfxCertificate -Password $secure_pw  -CertStoreLocation Cert:\LocalMachine\Root -FilePath certs.pfx;
