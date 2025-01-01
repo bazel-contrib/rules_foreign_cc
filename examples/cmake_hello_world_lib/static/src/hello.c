1ba5159e2d74b8d02c333493324c502d8955c92d
UebelAndre <github@uebelandre.com>
2025-01-01 15:28:22 -0800

    Added formatting checks for C/C++ and python code (#1348)

--- examples/cmake_hello_world_lib/static/src/hello.c
+++ examples/cmake_hello_world_lib/static/src/hello.c
@@ -1,7 +1,7 @@
 #include "hello.h"
 
 void hello_func(void) {
-	printf("Hello World!\n");
+    printf("Hello World!\n");
 
-	return;
+    return;
 }
--- examples/make_simple/code/liba.cpp
+++ examples/make_simple/code/liba.cpp
@@ -7,13 +7,11 @@
 #error "REQUIRED_DEFINE is not defined"
 #endif
 
-std::string hello_liba(void) {
-	return "Hello from LIBA!";
-}
+std::string hello_liba(void) { return "Hello from LIBA!"; }
 
 double hello_math(double a) {
-	// On Unix, this call requires linking to libm.so. The Bazel toolchain adds
-	// the required `-lm` linkopt automatically and rules_foreign_cc forwards
-	// this option to make invocation via the CXXFLAGS variable.
-	return acos(a);
+    // On Unix, this call requires linking to libm.so. The Bazel toolchain adds
+    // the required `-lm` linkopt automatically and rules_foreign_cc forwards
+    // this option to make invocation via the CXXFLAGS variable.
+    return acos(a);
 }
