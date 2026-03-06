d64ed9ca0f7a8fae85f6903054b4f074b73aa0f1
Mike Lundy <milundy@cisco.com>
2026-03-05 20:41:01 -0800

    fix cmake examples for windows

--- examples/cmake_defines/lib_a/lib_a.cpp
+++ examples/cmake_defines/lib_a/lib_a.cpp
@@ -4,12 +4,15 @@
 
 #define XSTR(x) STR(x)
 #define STR(x) #x
-#pragma message "The value of __TIME__: " XSTR(__TIME__)
+#pragma message("The value of __TIME__: " XSTR(__TIME__))
 
 #define STATIC_ASSERT(condition, name) \
     typedef char assert_failed_##name[(condition) ? 1 : -1];
 
-void foo() { STATIC_ASSERT(__TIME__ == "redacted", time_must_be_redacted); }
+// Verify __TIME__ was replaced with "redacted" by the build system.
+// A real __TIME__ is always "HH:MM:SS" starting with a digit; "redacted" starts
+// with 'r'.
+void foo() { STATIC_ASSERT(__TIME__[0] == 'r', time_must_be_redacted); }
 
 // Should return "redacted"
 const char *getBuildTime(void) { return __TIME__; }
