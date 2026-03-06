914450a0dc7622eb65b359aca380cddb66d1dcbe
Mike Lundy <milundy@cisco.com>
2026-03-05 20:38:12 -0800

    fix ninja and sync simple examples
    ninja and make fit roughly into the same category of build system, but
    we were only setting up the C build variables for make. This finishes
    the ninja implementation to match make (more or less) and then syncs the
    simple examples to match. The simple examples that don't already have
    compiler integrations (i.e. make and ninja) now use the same wrapper,
    which loads data from the configured toolchain instead of calling host
    binaries.

---
+++ examples/cc_wrapper.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+# This wrapper exists to provide a simplified way to wrap the compiler to do
+# the simple examples (make/ninja) that don't have the capability of doing
+# their own compiler setup. This exists _just_ to demonstrate and test
+# functionality for these simple builds, and if you're doing something real,
+# for your sake, use a full cross-platform build system like cmake and _don't_
+# use this script :)
+
+set -euo pipefail
+set -x
+
+die() {
+    echo "$*" >&2
+    exit 42
+}
+
+ensure_absolute() {
+    case "$1" in
+        /*|[A-Za-z]:/*)
+            # already absolute (maybe with a drive path)
+            echo "$1"
+            ;;
+        *)
+            echo "$EXT_BUILD_ROOT/$1"
+            ;;
+    esac
+}
+
+compile() {
+    if [[ -z "${CXX:-}" ]]; then
+        die "CXX is not set"
+    fi
+
+    local cxx
+    cxx="$(ensure_absolute "$CXX")"
+
+    case "$cxx" in
+        */cl.exe)
+            "$cxx" /c "$1" ${CXXFLAGS:-} "/Fo$2"
+            ;;
+        *)
+            "$cxx" -c "$1" ${CXXFLAGS:-} -o "$2"
+            ;;
+    esac
+}
+
+static_link() {
+    if [[ -z "${AR:-}" ]]; then
+        die "AR is not set"
+    fi
+
+    local ar
+    ar="$(ensure_absolute "$AR")"
+
+    case "$ar" in
+        */lib.exe)
+            "$ar" ${ARFLAGS:-} "/OUT:$2" "$1"
+            ;;
+        *)
+            "$ar" rcs ${ARFLAGS:-} "$2" "$1"
+            ;;
+    esac
+}
+
+if [[ -z "${EXT_BUILD_ROOT:-}" ]]; then
+    die "EXT_BUILD_ROOT is not set"
+fi
+
+if [[ "$#" -ne 3 ]]; then
+    echo "$0 <verb> <input> <output>"
+fi
+
+verb=$1
+input=$2
+output=$3
+
+case "$verb" in
+    compile) compile "$input" "$output" ;;
+    static_link) static_link "$input" "$output" ;;
+    *) die "unknown verb: $verb" ;;
+esac
