251fc08441d367bfef5424b9604d2cb95c390d91
Mike Lundy <mlundy@splunk.com>
2026-03-10 04:10:13 -0700

    examples: fix cmake examples for windows (#1479)
    1. Visual Studio appears to require the pragma message to be
    parenthesized.
    2. Equality of string constants relied on luck since it was actually
    comparing pointers.
    3. The rules_cc toolchains do not override `__TIME__` for visual studio.
    (They try, but visual studio does not allow you to override built-in
    constants with `-D`; you have to do it with a forced-include header).

--- examples/cmake_defines/lib_a/lib_a.cpp
+++ examples/cmake_defines/lib_a/lib_a.cpp
@@ -4,12 +4,22 @@
 
 #define XSTR(x) STR(x)
 #define STR(x) #x
-#pragma message "The value of __TIME__: " XSTR(__TIME__)
+#pragma message("The value of __TIME__: " XSTR(__TIME__))
 
 #define STATIC_ASSERT(condition, name) \
     typedef char assert_failed_##name[(condition) ? 1 : -1];
 
-void foo() { STATIC_ASSERT(__TIME__ == "redacted", time_must_be_redacted); }
+// Verify __TIME__ was replaced with "redacted" by the build system.
+// A real __TIME__ is always "HH:MM:SS" starting with a digit; "redacted" starts
+// with 'r'.
+#if defined(_MSC_VER)
+// MSVC does not honor overriding __TIME__ via compiler definitions (you have to
+// use a forced include for that, and the default rules_cc toolchain doesn't do
+// that)
+void foo() {}
+#else
+void foo() { STATIC_ASSERT(__TIME__[0] == 'r', time_must_be_redacted); }
+#endif
 
 // Should return "redacted"
 const char *getBuildTime(void) { return __TIME__; }
